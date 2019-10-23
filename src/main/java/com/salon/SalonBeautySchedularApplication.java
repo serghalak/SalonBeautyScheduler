38635f8787b425d2496caaab14c0162be1fb78ec
Serhiy Khalak <serghalak@gmail.com>
2019-10-23 13:44:59 +0300

    first commit

---
+++ src/main/java/com/salon/SalonBeautySchedularApplication.java
@@ -0,0 +1,13 @@
+package com.salon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalonBeautySchedularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalonBeautySchedularApplication.class, args);
+	}
+
+}
